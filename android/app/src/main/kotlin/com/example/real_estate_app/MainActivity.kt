9da6cb996d5290c0cce9dcf14b8193c6a7420400
Sizwe1806 <222078272@stud.cut.ac.za>
2024-10-20 22:07:28 +0100

    Make changes to my App

--- android/app/src/main/kotlin/com/example/real_estate_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/real_estate_app/MainActivity.kt
@@ -1,5 +1,11 @@
 package com.example.real_estate_app
 
 import io.flutter.embedding.android.FlutterActivity
-
+import io.flutter.plugins.pathprovider.PathProviderPlugin;
 class MainActivity: FlutterActivity()
+
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(savedInstanceState);
+  PathProviderPlugin.registerWith(registrarFor("io.flutter.plugins.pathprovider.PathProviderPlugin"));
+}
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import PathProvider
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -8,6 +9,7 @@ import UIKit
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    FLTPathProviderPlugin.register(with: registrarFor("io.flutter.plugins.pathprovider.PathProviderPlugin"))
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
